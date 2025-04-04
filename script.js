b64470a7998f7518b5f7efcb3236b138c0af0eb8
beglaryan3 <sargisbeglaryan3@gmail.com>
2025-04-04 12:44:54 +0400

    verj

---
+++ script.js
@@ -0,0 +1,445 @@
+const categoryList = {
+    "bracelet gold": {
+        image: [
+            "bracelet1.jpg",
+            "bracelet2.jpg",
+            "bracelet3.jpg",
+            "bracelet4.jpg",
+            "bracelet5.jpg",
+            "bracelet6.jpg",
+            "bracelet7.jpg",
+            "bracelet8.jpg",
+            "bracelet9.jpg",
+            "bracelet10.jpg"
+        ],
+        name: [
+            "LTK Group",
+            "Karina",
+            "Sweetv Kristal",
+            "Cerslimo",
+            "Jual Lanmi",
+            "Bracciale Donna",
+            "Liul Bijoux",
+            "Enamour Group",
+            "JV Mae Variedades",
+            "Pitani",
+        ],
+        price: [
+            "$15,00",
+            "$30,00",
+            "$21,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$20,00",
+            "$45,00",
+            "$12,00",
+            "$50,00",
+        ]
+    },
+    // 
+    "bracelet silver": {
+        image: [
+            "bracelet1.jpg",
+            "bracelet2.jpg",
+            "bracelet3.jpg",
+            "bracelet4.jpg",
+            "bracelet5.jpg",
+            "bracelet6.jpg",
+            "bracelet7.jpg",
+            "bracelet8.jpg",
+            "bracelet9.jpg",
+            "bracelet10.jpg"
+        ],
+        name: [
+            "Swarovski Angelic",
+            "Fairygate",
+            "Natalia CZ",
+            "Flora Perlen",
+            "Daiwujan",
+            "Urban Echo",
+            "Swarovski Lifelong Heart",
+            "Swarovski  Lifelong Gold Heart",
+            "Swarovski  Mesmera",
+            "Graff Laurence"
+        ],
+        price: [
+            "$138,00",
+            "$15,00",
+            "$56,00",
+            "$53,00",
+            "$59,00",
+            "$18,00",
+            "$89,00",
+            "$88,00",
+            "$149,00",
+            "$33000,00"
+        ]
+    },
+    // 
+    earring: {
+        image: [
+            "earring1.jpg",
+            "earring2.jpg",
+            "earring3.jpg",
+            "earring4.jpg",
+            "earring5.jpg",
+            "earring6.jpg",
+            "earring7.jpg",
+            "earring8.jpg",
+            "earring9.jpg",
+            "earring10.jpg"
+        ],
+        name: [
+            "H&M Ladies",
+            "Noco The Brand",
+            "Daiwujan",
+            "JV Mae Variedades",
+            "Mesmera",
+            "Swarovski",
+            "Laurence",
+            "Urban Echo",
+            "Fairygate",
+            "Daiwujan"
+        ],
+        price: [
+            "$83,00",
+            "$54,00",
+            "$476,00",
+            "$129,00",
+            "$179,00",
+            "$323,00",
+            "$19,00",
+            "$45,00",
+            "$35,00",
+            "$78,00"
+        ]
+    },
+    // 
+    anklet: {
+        image: [
+            "kette1.jpg",
+            "kette2.jpg",
+            "kette3.jpg",
+            "kette4.jpg",
+            "kette5.jpg",
+            "kette6.jpg",
+            "kette7.jpg",
+            "kette8.jpg",
+            "kette9.jpg",
+            "kette10.jpg"
+        ],
+        name: [
+            "Urban Echo",
+            "Swarovski Angelic",
+            "Daiwujan",
+            "JV Mae Variedades",
+            "Natalia CZ",
+            "Mesmera",
+            "JV Mae Variedades",
+            "Fairygate",
+            "Laurence",
+            "Mesmera"
+        ],
+        price: [
+            "$29,00",
+            "$25,00",
+            "$46,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$29,00",
+            "$45,00",
+            "$65,00",
+            "$78,00"
+        ]
+    },
+    // 
+    "ring gold": {
+        image: [
+            "ring1.jpg",
+            "ring2.jpg",
+            "ring3.jpg",
+            "ring4.jpg",
+            "ring5.jpg",
+            "ring6.jpg",
+            "ring7.jpg",
+            "ring8.jpg",
+            "ring9.jpg",
+            "ring10.jpg"
+        ],
+        name: [
+            "Natalia CZ",
+            "Urban Echo",
+            "Noco The Brand",
+            "JV Mae Variedades",
+            " Kristal",
+            "Fairygate",
+            "Swarovski Angelic",
+            "Sweetv Kristal",
+            "Mesmera",
+            "Laurence",
+        ],
+        price: [
+            "$29,00",
+            "$25,00",
+            "$46,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$29,00",
+            "$45,00",
+            "$65,00",
+            "$78,00",
+        ]
+    },
+    // 
+    "ring silver": {
+        image: [
+            "ring1.jpg",
+            "ring2.jpg",
+            "ring3.jpg",
+            "ring4.jpg",
+            "ring5.jpg",
+            "ring6.jpg",
+            "ring7.jpg",
+            "ring8.jpg",
+            "ring9.jpg",
+            "ring10.jpg",
+        ],
+        name: [
+            "Fairygate",
+            "Daiwujan",
+            "Urban Echo",
+            "Noco The Brand",
+            "Natalia CZ",
+            "JV Mae Variedades",
+            "Swarovski Angelic",
+            "Mesmera",
+            "Laurence",
+            "Daiwujan"
+        ],
+        price: [
+            "$29,00",
+            "$25,00",
+            "$46,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$29,00",
+            "$45,00",
+            "$65,00",
+            "$78,00"
+        ]
+    },
+    // 
+    "kette gold": {
+        image: [
+            "kette1.jpg",
+            "kette2.jpg",
+            "kette3.jpg",
+            "kette4.jpg",
+            "kette5.jpg",
+            "kette6.jpg",
+            "kette7.jpg",
+            "kette8.jpg",
+            "kette9.jpg",
+            "kette10.jpg"
+        ],
+        name: [
+            "Mesmera",
+            "Laurence",
+            "Daiwujan",
+            "Urban Echo",
+            "Noco The Brand",
+            "Natalia CZ",
+            "Swarovski Angelic",
+            "Fairygate",
+            "LTK Group",
+            "Sweetv Kristal"
+        ],
+        price: [
+            "$29,00",
+            "$25,00",
+            "$46,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$29,00",
+            "$45,00",
+            "$65,00",
+            "$78,00"
+        ]
+    },
+    // 
+    "kette silver": {
+        image: [
+            "kette1.jpg",
+            "kette2.jpg",
+            "kette3.jpg",
+            "kette4.jpg",
+            "kette5.jpg",
+            "kette6.jpg",
+            "kette7.jpg",
+            "kette8.jpg",
+            "kette9.jpg",
+            "kette10.jpg"
+        ],
+        name: [
+            "Swarovski Angelic",
+            "JV Mae Variedades",
+            "Noco The Brand",
+            "Laurence",
+            "Daiwujan",
+            "Swarovski Angelic",
+            "Sweetv Kristal",
+            "Natalia CZ",
+            "Urban Echo",
+            "Fairygate"
+        ],
+        price: [
+            "$29,00",
+            "$25,00",
+            "$46,00",
+            "$129,00",
+            "$19,00",
+            "$33,00",
+            "$29,00",
+            "$45,00",
+            "$65,00",
+            "$78,00"
+        ]
+    }
+    // 
+}
+
+const productCategoryNames = document.querySelectorAll(".section1_item")
+const productContainer = document.querySelector(".section2")
+const cartIcon = document.querySelector(".cart_icon")
+const cartContainer = document.querySelector(".cart_container")
+const cartClose = document.querySelector(".cart_close")
+const cartList = document.querySelector(".cart_list")
+const cartItemQuantity = document.querySelector(".total_itemQuantity")
+const cartTotalPrice = document.querySelector(".total_price")
+
+
+productCategoryNames.forEach(category => {
+
+    category.addEventListener("click", function () {
+        productCategoryNames.forEach(item => item.classList.remove("active"))
+        let categoryName = category.textContent.toLowerCase()
+        createProducts(categoryName)
+        category.classList.add("active")
+    })
+})
+
+function createProducts(category) {
+    let newCategory = categoryList[category]
+    let newImages = newCategory.image
+    let newNames = newCategory.name
+    let newPrices = newCategory.price
+
+    productContainer.innerHTML = newImages.map((item, index) => {
+        return `
+         <div class="product_item">
+            <div class="product_imageWrapper">
+                <img src="${category}/${item}" alt="" class="product_image">
+                <button class="addToCart">Add To Cart</button>
+            </div>
+            <h3 class="product_name">${newNames[index]}</h3>
+            <p class="product_price">${newPrices[index]}</p>
+        </div>
+        `
+    }).join("")
+
+    const addToCartButtons = document.querySelectorAll(".addToCart")
+    addToCartButtons.forEach(btn => {
+        btn.addEventListener("click", function () {
+            const btnParent = btn.closest(".product_item")
+            createCartItem(btnParent)
+            updatePrice()
+        })
+    })
+}
+createProducts("anklet")
+
+cartIcon.onclick = () => cartContainer.classList.toggle("show")
+cartClose.onclick = () => cartContainer.classList.remove("show")
+
+function createCartItem(parentElement) {
+    const imageSrc = parentElement.querySelector(".product_image").src
+    const name = parentElement.querySelector(".product_name").textContent
+    const price = parentElement.querySelector(".product_price").textContent
+
+    cartList.innerHTML += `
+    <li class="cart_item">
+        <div class="cart_imageWrapper">
+            <img src="${imageSrc}" alt="" class="cart_image">
+        </div>
+        <div class="cart_itemInfo">
+            <div>
+                <p class="cart_itemName">${name}</p>
+                <p class="cart_itemPrice">${price}</p>
+            </div>
+            <div class="cart_itemCuantity">
+                <p class="cart_itemText">QTY:</p>
+                <span class="cuantity_minus">-</span>
+                <span class="quantity">1</span>
+                <span class="quantity_plus">+</span>
+            </div>
+        </div>
+        <span class="cart_delete">
+            <i class="fa fa-times" aria-hidden="true"></i>
+        </span>
+    </li>
+    `
+    const cartItemDelete = document.querySelectorAll(".cart_delete")
+
+    cartItemDelete.forEach(x => {
+        x.addEventListener("click", function () {
+            const parentElement = x.closest(".cart_item")
+            parentElement.remove()
+            updatePrice()
+        })
+    })
+
+    const quantityBox = document.querySelectorAll(".cart_itemCuantity")
+    quantityBox.forEach(box => {
+        box.addEventListener("click", function (event) {
+            updateQuantity(event.target)
+        })
+    })
+
+}
+
+
+function updatePrice() {
+    let items = [...cartList.children]
+    cartItemQuantity.textContent = items.length + " Items"
+
+
+    let allPrices = items.map(item => {
+        const itemPrice = item.querySelector(".cart_itemPrice").textContent
+        const newPrice = parseFloat(itemPrice.replace("$", "").replace(",", "."))
+        const quantity = Number(item.querySelector(".quantity").textContent)
+        return newPrice * quantity
+    }).reduce((acc, price) => acc + price, 0)
+
+    cartTotalPrice.textContent = "$ " + allPrices
+
+}
+
+function updateQuantity(element) {
+    const parentElement = element.closest(".cart_itemCuantity")
+    const quantity = parentElement.querySelector(".quantity")
+    let quantityValue = parseInt(quantity.textContent)
+
+    if (element.classList.contains("quantity_plus")) {
+        quantityValue++
+        quantity.textContent = quantityValue
+    } else if (element.classList.contains("cuantity_minus") && quantityValue > 1) {
+        quantityValue--
+        quantity.textContent = quantityValue
+    }
+    updatePrice()
+}
